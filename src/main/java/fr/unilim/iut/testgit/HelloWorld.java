be12bccee891896363f24e9be66b20358bf9a0c8
Maxime <gitgud@ds3.com>
2018-04-26 10:42:04 +0200

    ajout main

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -1,9 +1,10 @@
 package fr.unilim.iut.testgit;
 
 public class HelloWorld {
-
-	public HelloWorld() {
-		// TODO Auto-generated constructor stub
-	}
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 
 }
